eb2f3d021108965b076897c5154d44562c28a187
lluquinhax <lucasfran1209@gmail.com>
2024-08-06 09:35:03 -0300

    v2.0

---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,12 @@
+import React, {useStat, useEffect} from 'react'
+
+const Listagem = () => {
+
+    return  (
+        <>
+        <h2>Pizzas ITB </h2>
+        </>
+    )
+}
+
+export default Listagem
