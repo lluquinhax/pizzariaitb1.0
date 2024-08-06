def8136f5c7072339c175f077682f2e4e7b8f170
lluquinhax <lucasfran1209@gmail.com>
2024-08-06 11:02:51 -0300

    v4

--- src/pages/Listagem.jsx
+++ src/pages/Listagem.jsx
@@ -1,12 +1,29 @@
 import React, {useState, useEffect} from 'react'
-
+import pizzas from '../shared/Pizzas'
 const Listagem = () => {
-
-    return  (
-        <>
-        <h2>Pizzas ITB </h2>
-        </>
-    )
+const ItensLista = () => pizzas.map(
+pizza =>
+<li key={pizza.id} style={{listStyle: 'none'}}>
+<img src={pizza.imagem}
+alt={pizza.nome}
+style={{
+width: "150",
+height: "100px",
+borderRadius: "8px",
+margin: "0px",
+}}
+/>
+</li>
+)
+return (
+<>
+<h2 style={{textAlign: 'center'}}>
+Pizzas ITB
+</h2>
+<ul>
+<ItensLista />
+</ul>
+</>
+)
 }
-
 export default Listagem
